0cfbbbddcbf55eb0f932a850c945dd7002e4be06
jacinda51 <jacindasuherly@gmail.com>
2022-11-26 13:31:58 +1100

    Revert "Week 04"
    This reverts commit 369046a7204f5591a45129787ebb8160a7bb5024.

--- app/src/main/java/au/edu/unsw/infs3634/cryptoprofile/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/cryptoprofile/MainActivity.java
@@ -1,41 +1,27 @@
 package au.edu.unsw.infs3634.cryptoprofile;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import au.edu.unsw.infs3634.cryptoprofile.api.Coin;
-import au.edu.unsw.infs3634.cryptoprofile.recyclerview_adapter.CoinAdapter;
-import au.edu.unsw.infs3634.cryptoprofile.recyclerview_adapter.RecyclerViewInterface;
-
-public class MainActivity extends AppCompatActivity implements RecyclerViewInterface {
+public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private Button btnLaunchActivity;
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // get the handle to the recycler view
-        recyclerView = findViewById(R.id.rvList);
-
-        // instantiate a linear recycler view layout manager
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        // instantiate the adapter and pass on the list of coins
-        adapter = new CoinAdapter(Coin.getCoins(), this);
-
-        // connect the adapter to the recycler view
-        recyclerView.setAdapter(adapter);
+        btnLaunchActivity = findViewById(R.id.btnLaunch);
+        btnLaunchActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchDetailActivity("ETH");
+            }
+        });
     }
 
     // Called when the user taps launch button
@@ -45,8 +31,4 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
         startActivity(intent);
     }
 
-    @Override
-    public void onItemClick(String symbol) {
-        launchDetailActivity(symbol);
-    }
 }
--- app/src/main/java/au/edu/unsw/infs3634/cryptoprofile/recyclerview_adapter/RecyclerViewInterface.java
+++
@@ -1,6 +0,0 @@
-package au.edu.unsw.infs3634.cryptoprofile.recyclerview_adapter;
-
-// interface to handle onClickListener for recycler view
-public interface RecyclerViewInterface {
-    void onItemClick(String symbol);
-}
