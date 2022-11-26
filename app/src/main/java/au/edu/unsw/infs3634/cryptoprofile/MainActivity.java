efc3fabc10f4cb4c2f3a843ead0feb30c9b34b14
jacinda51 <jacindasuherly@gmail.com>
2022-11-26 13:32:00 +1100

    Revert "Week 03"
    This reverts commit 4a6f6803ea8b411f89eee448ebd4841fead4acce.

--- app/src/main/java/au/edu/unsw/infs3634/cryptoprofile/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/cryptoprofile/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         btnLaunchActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                launchDetailActivity("ETH");
+                launchDetailActivity("Message from MainActivity");
             }
         });
     }
